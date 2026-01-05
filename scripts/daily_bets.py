c72914422f2599d4e65021b69a874740cc90ef9b
Patvath88 <patvath8@gmail.com>
2026-01-05 14:45:00 -0500

    Delete scripts directory

--- scripts/daily_bets.py
+++
@@ -1,7 +0,0 @@
-def generate_bets(df):
-    bets = df[
-        (df["EDGE"] > 0.03) &
-        (df["KELLY"] > 0)
-    ].sort_values("EDGE", ascending=False)
-
-    bets.to_csv("data/daily_bets.csv", index=False)
--- scripts/monte_carlo.py
+++
@@ -1,7 +0,0 @@
-import numpy as np
-
-def monte_carlo_prob(player_df, stat, minutes, line, n=20000):
-    per_min = (player_df[stat] / player_df["MIN"]).dropna()
-    samples = np.random.choice(per_min, size=n, replace=True)
-    sims = samples * minutes
-    return np.mean(sims > line)
